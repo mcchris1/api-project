28ddb993d7b82b60485135ace258bfa84a7a36f8
michae.lchristenson726@yahoo.com <121463561+mcchris1@users.noreply.github.com>
2023-04-12 12:12:56 -0400

    updates routes

--- routes/teams.js
+++ routes/teams.js
@@ -4,9 +4,9 @@ import * as controllers from "../controllers/teams.js";
 const router = Router();
 
 router.get("/", controllers.getTeams);
-router.get("/:id", controllers.getTeam);
+router.get("/:name", controllers.getTeam);
 router.post("/", controllers.createTeam);
-router.put("/:id", controllers.updateTeam);
-router.delete("/:id", controllers.deleteTeam);
+router.put("/:name", controllers.updateTeam);
+router.delete("/:name", controllers.deleteTeam);
 
 export default router;
